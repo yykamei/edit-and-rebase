90c81187b57d68deb3c1a1bff732957f4f20b030
Yutaka Kamei <kamei@yykamei.me>
2025-07-09 10:31:58 +0900

    Add initial implementation of GitHub Action in index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+
+try {
+    core.info("Hello");
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    core.info(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
