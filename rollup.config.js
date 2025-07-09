4a9d91ac59b7839495670f781678fbeac3a2938d
Yutaka Kamei <kamei@yykamei.me>
2025-07-09 10:41:19 +0900

    Add Rollup configuration for building the project

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+const config = {
+    input: "src/index.js",
+    output: {
+        esModule: true,
+        file: "dist/index.js",
+        format: "es",
+        sourcemap: true,
+    },
+};
+
+export default config;
